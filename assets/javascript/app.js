e338b9bae14e182d5941fe494d9a5da49e6f5145
Jack <jdempsey1993@gmail.com>
2019-07-23 18:14:49 -0400

    Updated html values

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,12 @@
 /// Set clock countdown
 
-    //Use a set interval function... 30 secound timeer
-
-    // If timer hits zero, show times up and answer correct and incorect
+    //Use a set interval function... 30 secound timer
+    setInterval(function(){ alert("Times Up"); }, 30000);
+    // If timer hits zero, submit values function
+    
+        //Alert times up 
+        
+        //Show answer correct and incorect
 
 
 /// When html submit, check user input values
