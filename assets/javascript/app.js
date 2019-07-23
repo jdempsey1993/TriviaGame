cfa0ede3c800039865299bedaa8d47bffdb5b710
Jack <jdempsey1993@gmail.com>
2019-07-23 18:03:19 -0400

    JS pseudocoding

---
+++ assets/javascript/app.js
@@ -0,0 +1,21 @@
+/// Set clock countdown
+
+    //Use a set interval function... 30 secound timeer
+
+    // If timer hits zero, show times up and answer correct and incorect
+
+
+/// When html submit, check user input values
+
+    // Could use an array?
+
+// Jquery alert to check values of answers
+
+    // If answer is correct, show correct to user
+
+   // If answer is false, show incorrect
+
+
+
+
+    
