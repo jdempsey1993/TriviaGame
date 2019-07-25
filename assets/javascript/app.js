4b1581a7089f1b8d46faf231d7188dbefcec1546
Jack <jdempsey1993@gmail.com>
2019-07-24 22:30:53 -0400

    fixed answer alert error

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,7 @@
 /// Set countdown function
 
     /// Set the timer to 30 seconds
+     function 
 
     /// Document innerHtml to #timer
 
@@ -8,7 +9,21 @@
 
         // When timer == submit answers
 
-    // Grab answers 
+    //Event to show the user how many answers correct
+    object.addEventListener("click", handleClick())
+    function handleClick() {
+        var amountCorrect = 0
+        for(var i = 1; i <=6; i++) {
+            var radios = document.getElementsByName("group"+i)
+            for (var j = 0; j <radios.length; j++) {
+                var radio = radios[j]
+                if (radio.value == "correct" && radio.checked) {
+                    amountCorrect++
+                }
+            }
+        } alert("Correct Responses: " + amountCorrect)
+    } 
+    
 
         // Check if wrong or correct against value wrong or correct
 
